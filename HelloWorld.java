fd6340419f32e30f5a93f28869373a7a94f7f267
Hamilton <ehamiltonhudson@gmail.com>
2019-09-01 20:12:38 -0400

    hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,8 @@
 public class HelloWorld {
-	public static void main(String[] args) {
+  public static void main(String[] args) {
     System.out.println("Java is a class-based language.");
     System.out.println("Java classes have a 'main' method.");
     System.out.println("Java statements end with a semicolon.");
-
     System.out.println("Programming is... fun!");
     // The main method executes the tasks of the class
     System.out.println("Hello Hamilton!");
