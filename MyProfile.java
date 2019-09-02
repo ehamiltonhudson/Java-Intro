52501c0972163109818076b5c3d241db9d7b0fa8
Hamilton <ehamiltonhudson@gmail.com>
2019-09-01 20:19:40 -0400

    MyProfile.java

---
+++ MyProfile.java
@@ -0,0 +1,11 @@
+public class MyProfile {
+  public static void main(String[] args) {
+    String name = "Hamilton";
+    int age = 30;
+    double lesson = 30.0;
+    char gender = 'F';
+    boolean lookingForJob = true;
+    System.out.println(name + ", " + age + ", " + gender);
+    System.out.println("Java lesson #: " + lesson);
+  }
+}
